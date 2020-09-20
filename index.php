e0a029e5cbbcf30820f140a1421c8173004c9c1c
Herson Lozano <hersonx@gmail.com>
2020-09-20 12:06:30 -0600

    actualizacionde archivo index.php

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GIT EN VSCODE</title>
-    <script type="text/javascript" src="app.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GIT EN VSCODE</title>
+  <script type="text/javascript" src="app.js"></script>
 </head>
+
 <body>
-<?php  
-for ($x = 10; $x <= 20; $x++) {
-  echo "Hola : $x <br>";
-}
-?> 
+  <?php  
+    for ($x = 10; $x <= 20; $x++) {
+      echo "Hola : $x <br>";
+    }
+  ?>
 </body>
-</html>
+
+</html>>
