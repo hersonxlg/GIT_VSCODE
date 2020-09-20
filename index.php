869c41ca805c5eec9528f8ecc9553a512bc54749
Herson Lozano <hersonx@gmail.com>
2020-09-20 02:13:30 -0600

    actualizar index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GIT EN VSCODE</title>
+    <script type="text/javascript" src="app.js"></script>
+</head>
 <body>
-
-<?php
+ <?php
 echo "My first PHP script!";
 ?>
-
 </body>
-</html> 
+</html>
